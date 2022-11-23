3f66c61a54bb4f84693304a954042130b098fb6a
taylankalkan01 <e.taylankalkan@hotmail.com>
2022-11-22 19:55:47 -0600

    Customer can Update Email.

--- controllers/customer/auth/updateEmail.js
+++ controllers/customer/auth/updateEmail.js
@@ -1,2 +1,35 @@
-const updateEmail = async (req, res) => {};
+const Customer = require("../../../models/Customer");
+const updateEmailValidation = require("../../../validations/customer/updateEmailValidation");
+
+const updateEmail = async (req, res) => {
+	try {
+		const { email } = req.body;
+
+		//validation email
+		const { error } = updateEmailValidation(email);
+		if (error) {
+			return res
+				.status(400)
+				.json({ error: true, message: error.details[0].message });
+		}
+
+		//update email
+		const data = await Customer.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: { email: email },
+			},
+			{
+				new: true,
+			}
+		);
+		res.status(200).json({
+			data: data._id,
+			message: `Email is updated: ${email}`,
+		});
+	} catch (err) {
+		res.status(500).send(err);
+		console.log(err);
+	}
+};
 module.exports = updateEmail;
