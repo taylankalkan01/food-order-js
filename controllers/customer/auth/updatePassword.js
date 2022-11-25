a1d9706d9e82d4e31d3febbf3caaa3c7f90fa47a
taylankalkan01 <e.taylankalkan@hotmail.com>
2022-11-25 10:40:59 -0600

    Customer can update password.

--- controllers/customer/auth/updatePassword.js
+++ controllers/customer/auth/updatePassword.js
@@ -1,2 +1,45 @@
-const updatePassword = async (req, res) => {};
+const Customer = require("../../../models/Customer");
+const updatePasswordValidation = require("../../../validations/customer/updatePasswordValidation");
+const bcrypt = require("bcrypt");
+
+const updatePassword = async (req, res) => {
+	try {
+		const { password } = req.body;
+		const { id } = req.params;
+
+		//validation password
+		const { error } = updatePasswordValidation(password);
+		if (error) {
+			return res
+				.status(400)
+				.json({ error: true, message: error.details[0].message });
+		}
+		//bcrypt password
+		const hashedPass = await bcrypt.hash(password, 10);
+
+		//compare old and new password
+		const user = await Customer.findById(id);
+		const oldPass = await bcrypt.compare(password, user.password);
+		if (oldPass)
+			return res.status(400).json({
+				error: true,
+				message: "New password cannot be the same as your old password",
+			});
+
+		//update password
+		const data = await Customer.findByIdAndUpdate(
+			id,
+			{
+				$set: { password: hashedPass },
+			},
+			{ new: true }
+		);
+		res.status(200).json({
+			data: data._id,
+			message: `Password is updated`,
+		});
+	} catch (err) {
+		res.status(500).send(err);
+	}
+};
 module.exports = updatePassword;
