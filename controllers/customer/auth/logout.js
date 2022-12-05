346ce34c9be199a05c82a85aa2078615ba1a974d
taylankalkan01 <e.taylankalkan@hotmail.com>
2022-12-04 22:18:57 -0600

    token fixed and logout api added.

--- controllers/customer/auth/logout.js
+++ controllers/customer/auth/logout.js
@@ -1,8 +1,8 @@
 const logout = async (req, res) => {
-	try {
-		res.clearCookie("jwt").status(200).json({ message: "Logout Successful." });
-	} catch (err) {
-		res.status(500).send(err);
-	}
+  try {
+    res.clearCookie("jwt").status(200).json({ message: "Logout Successful." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 module.exports = logout;
--- middlewares/VerifyToken.js
+++ middlewares/VerifyToken.js
@@ -1,21 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-	try {
-		const authHeader =
-			req.headers.token || req.header("token") || req.cookies.jwt;
-		if (!authHeader?.startsWith("Bearer ")) {
-			return res
-				.status(401)
-				.json({ error: true, message: "Unauthorized, Missing Bearer" });
-		}
-		const token = authHeader.split(" ")[1];
+  try {
+    const auth = req.cookies.jwt;
 
-		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_PRIVATE_KEY);
-		req.user = decoded.user;
-		next();
-	} catch (err) {
-		res.status(500).send(err);
-	}
+    if (!auth) {
+      return res
+        .status(403)
+        .json({ error: true, message: "Token is missing." });
+    }
+
+    const decoded = jwt.verify(auth, process.env.ACCESS_TOKEN_PRIVATE_KEY);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 module.exports = verifyToken;
