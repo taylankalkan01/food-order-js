1ee00e07333ee5633f56c2e60627bc0f9bd088af
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-01-03 11:32:46 +0300

    e2e and some model fixed.

--- __tests__/customer/register.test.js
+++ __tests__/customer/register.test.js
@@ -1,18 +1,19 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3000/api";
-// const mongoose = require("mongoose");
-// const Customer = require("../../models/Customer");
+const mongoose = require("mongoose");
+const Customer = require("../../models/Customer");
 
 // jest.setTimeout(50000);
 
-// beforeAll(async () => {
-// 	const url = process.env.MONGO_URI;
-// 	await mongoose.connect(url, { useNewUrlParser: true });
-// });
-// // Cleans up database between each test
-// afterEach(async () => {
-// 	await Customer.deleteMany();
-// });
+beforeAll(async () => {
+  const url =
+    "mongodb+srv://admin:admin123@cluster0.r91agvy.mongodb.net/?retryWrites=true&w=majority";
+  await mongoose.connect(url, { useNewUrlParser: true });
+});
+// Cleans up database between each test
+afterEach(async () => {
+  await Customer.deleteMany();
+});
 
 // afterAll(async () => {
 // 	// Removes the User collection
@@ -21,38 +22,71 @@ const baseURL = "http://localhost:3000/api";
 // });
 
 describe("customer register api", () => {
-	// it("should return status 400 if validation is not correct",async()=>{
-	// })
-	// it("should return error message if validation is not correct",async()=>{
-	// })
-	// it("should return status 400 if email exist in db",async()=>{
-	// })
-	// it("should return error message if email exist in db",async()=>{
-	// })
-	// it("should return 201 if user created", async (done) => {
-	// 	const data = {
-	// 		fullName: "test1",
-	// 		email: "test1@gmail.com",
-	// 		phone: "5555555555",
-	// 		password: "test1",
-	// 	};
-	// 	const response = await request(baseURL)
-	// 		.post("/auth/customer/register")
-	// 		.send(data);
-	// 	expect(response.statusCode).toEqual(201);
-	// });
-	// it("should return json data", async (done) => {
-	// 	const data = {
-	// 		fullName: "test1",
-	// 		email: "test1@gmail.com",
-	// 		phone: "5555555555",
-	// 		password: "test1",
-	// 	};
-	// 	const response = await request(baseURL)
-	// 		.post("/auth/customer/register")
-	// 		.send(data);
-	// 	expect(response.body).toEqual(data);
-	// });
-	// it("should return message if user created",async()=>{
-	// })
+  //   it("should return status 400 if fullname validation is not correct",async()=>{
+  // 	const data = {
+  // 				fullName: "test1",
+  // 				email: "test1@gmail.com",
+  // 				phone: "5555555555",
+  // 				password: "test1",
+  // 			};
+  //   })
+  //   it("should return status 400 if email validation is not correct",async()=>{
+  // 	const data = {
+  // 				fullName: "test1",
+  // 				email: "test1@gmail.com",
+  // 				phone: "5555555555",
+  // 				password: "test1",
+  // 			};
+  //   })
+  //   it("should return status 400 if phone validation is not correct",async()=>{
+  // 	const data = {
+  // 				fullName: "test1",
+  // 				email: "test1@gmail.com",
+  // 				phone: "5555555555",
+  // 				password: "test1",
+  // 			};
+  //   })
+  // it("should return error message if validation is not correct",async()=>{
+  // })
+  // it("should return status 400 if email exist in db",async()=>{
+  // })
+  // it("should return error message if email exist in db",async()=>{
+  // })
+  //   it("should return 201 if user created", async () => {
+  //     const data = {
+  //       fullName: "test1",
+  //       email: "test1234@gmail.com",
+  //       phone: "5555555555",
+  //       password: "test1",
+  //     };
+  //     const response = await request(baseURL)
+  //       .post("/auth/customer/register")
+  //       .send(data);
+  //     console.log(response);
+  //     expect(response.statusCode).toEqual(201);
+  //   });
+  it("should return json data", async () => {
+    const data = {
+      fullName: "test1",
+      email: "test1434@gmail.com",
+      phone: "5555555555",
+      password: "test1",
+    };
+    const response = await request(baseURL)
+      .post("/auth/customer/register")
+      .send(data);
+    console.log(response);
+    expect(Object.keys(response.body.data)).toEqual([
+      "email",
+      "password",
+      "fullName",
+      "phone",
+      "order",
+      "_id",
+      "cart",
+      "createdAt",
+      "updatedAt",
+    ]);
+  });
+  it("should return message if user created", async () => {});
 });
--- controllers/customer/restaurant/getRestaurantByName.js
+++ controllers/customer/restaurant/getRestaurantByName.js
@@ -1,2 +1,11 @@
-const getRestaurantByName = async (req, res) => {};
+const Restaurant = require("../../../models/Restaurant");
+const getRestaurantByName = async (req, res) => {
+  try {
+    // const data = await Restaurant.findOne({ name: req.body.name });
+    const data = await Restaurant.find({ name: req.body.name });
+    res.status(200).json({ data: data, message: "All Restaurant is listed." });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
 module.exports = getRestaurantByName;
